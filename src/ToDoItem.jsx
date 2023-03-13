0da31cb1a43fefdc91680c32a7fbf8a480add79b
Zarin Tasmin Sejuti <zarintasmin@Zarins-MacBook-Air.local>
2023-03-13 12:09:38 +0200

    text decoration line through over the text

--- src/ToDoItem.jsx
+++ src/ToDoItem.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 
 function ToDoItem(props){
-    return <li>{props.text}</li>;
+
+    const [isDone, setIsDone] = useState(false);
+    function handleClick(){
+        setIsDone((prevValue) => {
+            return !prevValue;
+         });
+    }
+    
+
+
+
+    <div onClick={handleClick}>
+    return <li style={{textDecoration: isDone ? 'line-through' : "none" }}>{props.text}</li>;
+    </div>
 }
 
 
